f77de1f926fd342036c8a1a5edf1bd3d3ca9df8d
fbouras-nova <farid.bouras@nova-technology.fr>
2023-09-03 12:14:22 +0200

    test:WIP => add function sortedByDate

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,11 +1,11 @@
-import VerticalLayout from './VerticalLayout.js'
-import ErrorPage from "./ErrorPage.js"
-import LoadingPage from "./LoadingPage.js"
+import VerticalLayout from './VerticalLayout.js';
+import ErrorPage from './ErrorPage.js';
+import LoadingPage from './LoadingPage.js';
 
-import Actions from './Actions.js'
+import Actions from './Actions.js';
 
 const row = (bill) => {
-  return (`
+  return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
@@ -16,16 +16,40 @@ const row = (bill) => {
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
-  }
+    `;
+};
 
-const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+function sortByDate(data) {
+  // Utilisez la méthode sort() avec une fonction de comparaison personnalisée
+  return data.sort(function (a, b) {
+    // Convertissez les dates en objets Date pour la comparaison
+    const dateA = a.date;
+    const dateB = b.date;
+
+    // Comparez les dates pour le tri décroissant
+    if (dateA > dateB) {
+      return -1;
+    } else if (dateA < dateB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
 }
 
+const rows = (data) => {
+  console.log('data before', data);
+  const dataSorted = sortByDate(data);
+  console.log('data after', data);
+
+  console.log('dataSorted', dataSorted);
+  return data && data.length
+    ? dataSorted.map((bill) => row(bill)).join('')
+    : '';
+};
+
 export default ({ data: bills, loading, error }) => {
-  
-  const modal = () => (`
+  const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -40,15 +64,15 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `;
 
   if (loading) {
-    return LoadingPage()
+    return LoadingPage();
   } else if (error) {
-    return ErrorPage(error)
+    return ErrorPage(error);
   }
-  
-  return (`
+
+  return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -75,6 +99,5 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
       ${modal()}
-    </div>`
-  )
-}
+    </div>`;
+};
