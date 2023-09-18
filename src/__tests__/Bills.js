4b993ba1326614917e37c35bb37d20bb7df7e9d4
fbouras-nova <farid.bouras@nova-technology.fr>
2023-09-18 15:44:51 +0200

    test: add Test "Eye" Icon Click and Test "New Bill" Button Click

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,20 +1,17 @@
-/**
- * @jest-environment jsdom
- */
-
 import { screen, waitFor } from '@testing-library/dom';
 import BillsUI from '../views/BillsUI.js';
 import { bills } from '../fixtures/bills.js';
 import { ROUTES_PATH } from '../constants/routes.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
-
 import router from '../app/Router.js';
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on Bills Page', () => {
-    test('Then bill icon in vertical layout should be highlighted', async () => {
+    beforeEach(() => {
+      // Configuration commune à tous les tests de cette section
       Object.defineProperty(window, 'localStorage', {
-        value: localStorageMock
+        value: localStorageMock,
+        writable: true
       });
       window.localStorage.setItem(
         'user',
@@ -27,11 +24,14 @@ describe('Given I am connected as an employee', () => {
       document.body.append(root);
       router();
       window.onNavigate(ROUTES_PATH.Bills);
+    });
+
+    test('Then bill icon in vertical layout should be highlighted', async () => {
       await waitFor(() => screen.getByTestId('icon-window'));
       const windowIcon = screen.getByTestId('icon-window');
-      //to-do write expect expression
       expect(windowIcon.className).toEqual('active-icon');
     });
+
     test('Then bills should be ordered from earliest to latest', () => {
       document.body.innerHTML = BillsUI({ data: bills });
       const dates = screen
@@ -42,6 +42,26 @@ describe('Given I am connected as an employee', () => {
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+
+    test('Then clicking on the "New Bill" button should navigate to the New Bill page', async () => {
+      const newBillButton = screen.getByTestId('btn-new-bill');
+      newBillButton.click();
+      await waitFor(() =>
+        // vérifier que l'URL contient bien le chemin souhaité
+        expect(window.location.href).toContain(ROUTES_PATH.NewBill)
+      );
+    });
+
+    test('Then clicking on the "Eye" icon should display the bill image in a modal', async () => {
+      const eyeIcons = screen.getAllByTestId('icon-eye');
+      const firstEyeIcon = eyeIcons[0];
+      firstEyeIcon.click();
+      //wait for permet d'attendre que la modale soit rendue dans le DOM
+      await waitFor(() => {
+        const modal = screen.getByRole('dialog');
+        expect(modal).toBeTruthy();
+      });
     });
   });
 });
